6ee2b3cc98882d3e2826c44874e83d73ad53bc86
jj1765 <124225031+jj1765@users.noreply.github.com>
2026-05-31 11:48:38 -0400

    Added card functionality
    Created a type that holds cards, information for front and back. Also created a form system to be used when adding a card to a deck

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,121 +2,117 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+type Card = {
+  front: string;
+  back: string;
+}
+type AddCardFormProps = {
+  onAdd: (card: Card) => void;
+};
+
+const cards: Card[] = [
+  {
+    front: "dom",
+    back: "house"
+  }, 
+  {
+    front: "kot",
+    back: "cat"
+  }, 
+  {
+    front: "pies",
+    back: "dog"
+  }, 
+]
+
+function addCard(card: Card){
+  cards.push(card);
+}
+function AddCardForm({onAdd}: AddCardFormProps){
+  const [front, setFront] = useState('');
+  const [back, setBack] = useState('');
+
+
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>){
+    e.preventDefault();
+
+    onAdd({front,back});
+    setFront('');
+    setBack('');
+
+  }
+    return(
+      <div>
+        <form onSubmit={handleSubmit}>
+          <h2>Add a Card</h2>
+
+          <input placeholder="Front" value={front} onChange={(e) => setFront(e.target.value)}></input>
+          <input placeholder="Back" value={back} onChange={(e) => setBack(e.target.value)}></input>
+
+          <button type="submit">Add</button>
+
+        </form>
+      </div>
+    )
+
+  }
+function CardTraining(){
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+  const hasNext = index < cards.length - 1;
+
+  function handleNextClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+      setShowMore(false);
+
+    } else {
+      setIndex(0);
+      setShowMore(false);
+    }
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+
+  let frontCard = cards[index].front;
+  let backCard = cards[index].back;
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div>
+      <button onClick={handleNextClick}> Next </button>
+      <h2>
+        <p>{showMore ? backCard : frontCard}</p>
+      </h2>
+      <button onClick={handleMoreClick}> Flip </button>
+      <h3>Card {index + 1 } of {cards.length}</h3>
+      <AddCardForm onAdd={addCard} />
 
-      <div className="ticks"></div>
+    </div>
+  );
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+  
 }
 
-export default App
+/*function App() {
+  
+
+
+
+  return (
+    <div>
+    <h1>FlashCards</h1>
+    <p>This site is designed to be used for memorizing cards for any use case.</p>
+    
+    <button>Click here for more</button>
+    </div>
+  )
+} */
+
+export default CardTraining
